115276068c9a1c9d087993eaf57f92b53c77f268
Piotr Siupa <piotr.siupa@student.put.poznan.pl>
2016-04-25 14:41:20 +0200

    Add comments and clean up.

--- asm_to_pas.pl
+++ asm_to_pas.pl
@@ -1,11 +1,18 @@
-%load input and save output
+%------------------------------------ MAIN -------------------------------------
+
 main() :-
 	read_stream_to_codes(user_input, Asm),
 	program(Pas, Asm, []),
 	writef("%s", [Pas]),
 	halt.
 
+
 %-------------------------------------------------------------------------------
+%--------------------------------- DCG  PARSER ---------------------------------
+%-------------------------------------------------------------------------------
+
+
+%----------------------------------- PROGRAM -----------------------------------
 
 program(Program) -->
 	program_header(Header),
@@ -16,6 +23,8 @@ program(Program) -->
 	end_non_significant_lines(),
 	{ append([Header, Constants, Main], Program) }.
 
+%----------------------------------- HEADER ------------------------------------
+
 program_header(Header) --> program_header_lines(), last_program_header_line(), { Header = "PROGRAM Prog;\n" }.
 program_header_lines() --> program_header_line(), program_header_lines().
 program_header_lines() --> "".
@@ -23,6 +32,8 @@ program_header_line() --> non_significant_line() | program_header_global_line() 
 program_header_global_line() --> maybe_whitespaces(), "global", whitespaces(), name(_), non_significant_thing(), "\n".
 program_header_extern_line() --> maybe_whitespaces(), "extern", whitespaces(), name(_), non_significant_thing(), "\n".
 last_program_header_line() --> maybe_whitespaces(), "section", whitespaces(),".text", non_significant_thing(), "\n".
+
+%---------------------------------- CONSTANTS ----------------------------------
 
 constants(Constants) --> constant_lines(ConstantLines), { append("\nCONST\n", ConstantLines, Constants) }.
 constants(Constants) --> "", { Constants = "" }.
@@ -32,25 +43,57 @@ constant_line(ConstantLine) --> label(Name), non_significant_lines(), maybe_whit
 constant_data(Data) --> constant_string(Data).
 %TODO more types
 
+%------------------------------- string constant -------------------------------
+
 constant_string(ConstantString) --> whitespaces(), number(_), constant_string_part(StringPart), { ConstantString = StringPart }.
 constant_string_part(StringPart) --> maybe_whitespaces(), ",", maybe_whitespaces(), number(Number), constant_string_part(NextStringPart), { append(["#", Number, NextStringPart], StringPart) }.
 constant_string_part(StringPart) --> maybe_whitespaces(), ",", maybe_whitespaces(), string(String), constant_string_part(NextStringPart), { append(["'", String, "'", NextStringPart], StringPart) }.
 constant_string_part(StringPart) --> "", { StringPart = "" }.
 
+string(String) --> "'", string_part(StringPart), "'", { String = StringPart }.
+string_part(StringPart) --> string_char(Char), string_part(NextStringPart), { append(Char, NextStringPart, StringPart) }.
+string_part(StringPart) --> "", { StringPart = "" }.
+string_char(Char) --> [C], { C \= '\n', C \= '\'', Char = [C] }.
+
+%---------------------------------- FUNCTIONS ----------------------------------
+
+function_header(Name) --> function_label([95|Name]).
+
+function_label(Name) --> label(Name).
+
+%-------------------------------- main function --------------------------------
+
 main_function(Main) --> main_function_header(), function_body(Body), { append(["\nBEGIN\n", Body, "END.\n"], Main) }.
 main_function_header() --> function_header("main").
 
-function_header(Name) --> label([95|Name]).
-
-label(Name) --> maybe_whitespaces(), name(Name), ":", non_significant_thing(), "\n".
+%-------------------------------- function body --------------------------------
 
 function_body(Body) --> function_line(Line), "\n", function_body(BodyPart), { append(Line, BodyPart, Body) }.
 function_body(Body) --> return_instruction(Return), "\n", { Body = Return }.
+
+%--------------------------------- INSTRUCTION ---------------------------------
 
 return_instruction(Return) --> maybe_whitespaces(), "ret", non_significant_thing(), { Return = "\n" }.
 	
 function_line(Line) --> non_significant_thing(), { Line = "" }.
 %TODO real instructions
+
+%------------------------------------ LABEL ------------------------------------
+
+label(Name) --> maybe_whitespaces(), name(Name), ":", non_significant_thing(), "\n".
+
+%------------------------------------ NAME -------------------------------------
+
+name(Name) --> first_name_character(Char), rest_of_name(RestOfName), { append(Char, RestOfName, Name) }. %not sure if it is good - check assembler documentation
+rest_of_name(Name) --> name_character(Char), rest_of_name(RestOfName), { append(Char, RestOfName, Name) }.
+rest_of_name(Name) --> "", { Name = "" }.
+
+name_character(Char) --> digit(Char) | first_name_character(Char).
+first_name_character(Char) --> letter(Char) | other_name_character(Char).
+other_name_character(Char) --> "_", { Char = "_" }.
+other_name_character(Char) --> ".", { Char = "." }.
+
+%----------------------------------- COMMENT -----------------------------------
 
 end_non_significant_lines() --> non_significant_line(), end_non_significant_lines().
 end_non_significant_lines() --> non_significant_thing().
@@ -61,26 +104,17 @@ non_significant_line() --> non_significant_thing(), "\n".
 non_significant_thing() --> maybe_whitespaces(), maybe_comment().
 
 maybe_comment() --> "" | comment().
-comment() --> ";", maybe_no_newline_characters().
+comment() --> ";", comment_body().
+comment_body() --> "" | no_newline_characters().
+
+%--------------------------------- WHITESPACES ---------------------------------
 
 maybe_whitespaces() --> "" | whitespaces().
 whitespaces() --> whitespace(), whitespaces().
 whitespaces() --> whitespace().
 whitespace() --> " " | "\t".
 
-name(Name) --> non_digit_name_character(Char), rest_of_name(RestOfName), { append(Char, RestOfName, Name) }. %not sure if it is good - check assembler documentation
-rest_of_name(Name) --> name_character(Char), rest_of_name(RestOfName), { append(Char, RestOfName, Name) }.
-rest_of_name(Name) --> "", { Name = "" }.
-
-name_character(Char) --> digit(Char) | non_digit_name_character(Char).
-non_digit_name_character(Char) --> letter(Char) | other_name_character(Char).
-other_name_character(Char) --> "_", { Char = "_" }.
-other_name_character(Char) --> ".", { Char = "." }.
-
-string(String) --> "'", string_part(StringPart), "'", { String = StringPart }.
-string_part(StringPart) --> string_char(Char), string_part(NextStringPart), { append(Char, NextStringPart, StringPart) }.
-string_part(StringPart) --> "", { StringPart = "" }.
-string_char(Char) --> [C], { C \= 10, C \= 39, Char = [C] }.
+%------------------------------- CHARACTER TYPES -------------------------------
 
 letter(Letter) --> [LetterCode], { code_type(LetterCode, alpha), Letter = [LetterCode] }.
 
@@ -88,9 +122,6 @@ number(Number) --> digit(Digit), number(NumberPart), { append(Digit, NumberPart,
 number(Number) --> "", { Number = "" }.
 digit(Digit) --> [DigitCode], { code_type(DigitCode, digit), Digit = [DigitCode] }.
 
-maybe_no_newline_characters() --> "" | no_newline_characters().
 no_newline_characters() --> no_newline_character(), no_newline_characters().
 no_newline_characters() --> no_newline_character().
-no_newline_character() --> [C], { not(head_is_the_same([C], "\n")) }.
-
-head_is_the_same([H1|_], [H2|_]) :- H1 == H2.
+no_newline_character() --> [C], { C \= '\n' }.
